fb885fc0dacfa2c26d0380061aa7cc0f2635f6e5
Mohammed Nazar <x3raqe@gmail.com>
2024-02-11 22:54:17 +0300

    Completed project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      content: {
+        'topsec': 'url("./images/bg-section-top-mobile-1.svg")',
+      },
+    },
+  },
+  plugins: [],
+}
+
